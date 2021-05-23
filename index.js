95b5be3dbcd03cf81cad3c68d95f9ee4bc465c77
lin-zhao-12 <83961735+lin-zhao-12@users.noreply.github.com>
2021-05-23 19:55:33 -0400

    js added

---
+++ index.js
@@ -0,0 +1,36 @@
+// ----------------Task 1 declare a, b, c-------------------------
+
+let a = 3;
+let b = 5;
+let c;
+
+let output1 = 'let a = 3;\nlet b = 5;\nlet c;\n------\n a + b = ' 
++ (a + b) + '\n a - b = ' 
++ (a - b) + '\n a * b = ' 
++ (a * b) + '\n a / b = '
++ (a / b) + '\n a % b = '
++ (a % b) + '\n a += b = '
++ (a += b) + '\n a -= b = '
++ (a -= b) + '\n a *= b = '
++ (a *= b) + '\n a /= b = '
++ (a /= b) + '\n a %= b = '
++ (a %= b) + '\n a == b = '
++ (a == b) + '\n a != b = '
++ (a != b) + '\n a > b = '
++ (a > b) + '\n a < b = '
++ (a < b) + '\n !a && !c  = '
++ (!a && !c) + '\n !a || !c  = '
++ (!a || !c)
+; 
+
+alert(output1);
+
+
+
+// ----------------Task 2 -------------------------
+
+let first_name = 'Lin';
+let last_name = 'Zhao';
+let email = 'zhao0208@algonquinlive.com';
+let output = "My name is " + first_name + " " + last_name + "." + " You can contact me at " + email + ".";
+alert(output);
